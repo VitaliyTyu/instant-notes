295fc13c363f084b6c0c8702c2c3a877311282db
Виталий Тютюников <vtyutyunikov@ekonomexpert.ru>
2024-08-11 19:14:59 +0500

    init

---
+++ main.js
@@ -0,0 +1,21 @@
+const { ipcMain } = require("electron");
+const { app, BrowserWindow } = require("electron/main");
+const path = require("node:path");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  ipcMain.handle("ping", () => "pong");
+});
