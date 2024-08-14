cfaae6d629029296c1fba853a16d11c4673eff39
Виталий Тютюников <vtyutyunikov@ekonomexpert.ru>
2024-08-14 15:12:10 +0500

    feat: add reactive hash

--- packages/renderer/src/components/ReactiveHash.tsx
+++ packages/renderer/src/components/ReactiveHash.tsx
@@ -1,7 +1,24 @@
+import {sha256sum} from '#preload';
+import {useEffect, useState} from 'react';
+
 function ReactiveHash() {
+  const [rawString, setRawString] = useState<string>('Hello world!');
+  const [hexString, setHexString] = useState<string>();
+
+  useEffect(() => {
+    const hash = sha256sum(rawString);
+    setHexString(hash);
+  }, [rawString]);
+
   return (
     <>
       <h1>Reactive Hash</h1>
+      <input
+        value={rawString}
+        onChange={v => setRawString(v.target.value)}
+      />
+      <p></p>
+      <label>{hexString}</label>
     </>
   );
 }
